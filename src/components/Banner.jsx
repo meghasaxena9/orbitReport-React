b4ae54b3f27f0252b1c89dcd9c1d648b77b1a76a
megha saxena <meghasaxena9@gmail.com>
2023-11-20 09:57:21 -0600

    message

---
+++ src/components/Banner.jsx
@@ -0,0 +1,8 @@
+export default function Banner () {
+  return(
+    <div>
+      <h2>Orbit Report</h2>
+      <p>Click on the buttons to see the satellites in that orbit type</p>
+    </div>
+  );
+}
--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,18 +1,37 @@
+//import satData from "./satData";
+
+//function satStatus(props){
+ // const issatStatus = props.issatStatus;
+  //return issatStatus ?  <td>Inactive</td> : <td>Active</td>
+//}
+
 const Table = ({ sat }) => {
+  let tabBody=sat.map((data, id) => {
+    const status = data.operational ? "Active" : "Inactive";
   return (
+    <tbody>
+     <tr key={id}>
+       <td>{data.name}</td>
+       <td>{data.type}</td>
+       <td>{data.launchDate}</td>
+       
+      <td>{status}</td> 
+     </tr>
+     </tbody>
+  );
+  });
+    let tab =
     <table>
     <thead>
      <tr>
-       <th>Header TBD</th>
+       <th>Name</th>
+       <th>Type of Satellite</th>
+       <th>Launch Date</th>
+       <th>Status</th>
      </tr>
      </thead>
-     <tbody>
-     <tr>
-       <td>Row Data TBD</td>
-     </tr>
-     </tbody>
+     {tabBody}
    </table>
-  );
+return tab
 };
-
 export default Table;
